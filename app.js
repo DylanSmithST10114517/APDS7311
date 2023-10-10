5ff235529293b8e733bfd4557d6e6949c53065f2
DylanSmith <ST10114517@vcconnect.edu.za>
2023-10-10 16:55:25 +0200

    full functional with comments

--- app.js
+++ app.js
@@ -1,31 +1,47 @@
+// Import required libraries
 const express = require('express');
 const app = express();
 const https = require('https');
 const fs = require('fs');
-const hsts = require('./middleware/hsts');
+const hsts = require('./middleware/hsts'); // Import HSTS middleware
 const mongoose = require('mongoose');
-const helmet = require('helmet');
-const cors = require('cors')
+const helmet = require('helmet'); // Import helmet for security headers
+const cors = require('cors'); // Import CORS middleware
 const connstring = 'mongodb+srv://ST10114517:pgPBm9NFR2yVS739@cluster0.0fq8rs5.mongodb.net/test';
 
-mongoose.connect(connstring)
+// Connect to MongoDB using the connection string
+mongoose.connect(connstring);
 
-//middleware
+// Middleware section
+
+// Enable security-related HTTP headers using helmet
 app.use(helmet());
-app.use(express.json());
-app.use(hsts);
-app.use(cors({origin: 'https://localhost:3000', optionsSuccessStatus: 200}));
 
-//routes
+// Parse JSON requests
+app.use(express.json());
+
+// Enable HTTP Strict Transport Security (HSTS) using the 'hsts' middleware
+app.use(hsts);
+
+// Configure CORS settings to allow requests from a specific origin
+app.use(cors({ origin: 'https://localhost:3000', optionsSuccessStatus: 200 }));
+
+// Routes section
+
+// Define routes for user-related functionality
 app.use('/api/user', require('./routes/user'));
+
+// Define routes for posts-related functionality
 app.use('/api/posts', require('./routes/posts'));
 
-//test get
+// Define a test route to check if the server is working
 app.get('/test', (req, res) => {
-    res.send('test worked')
-})
+    res.send('test worked');
+});
 
-//server
+// Server section
+
+// Create an HTTPS server using provided key and certificate, and listen on port 3000
 https.createServer({
     key: fs.readFileSync('./keys/privatekey.pem'),
     cert: fs.readFileSync('./keys/certificate.pem')
--- utils/hash.js
+++ utils/hash.js
@@ -1,12 +1,23 @@
+// Import the 'bcrypt' library for password hashing and validation
 const bcrypt = require('bcrypt');
 
-async function hashPassword(password){
+// Function to hash a password using bcrypt
+async function hashPassword(password) {
+    // Generate a salt for hashing with 12 rounds of salting
     const salt = await bcrypt.genSalt(12);
-    const hashed = bcrypt.hash(password,salt);
+    
+    // Hash the password using the generated salt
+    const hashed = await bcrypt.hash(password, salt);
+    
+    // Return the hashed password
     return hashed;
 }
-async function isValidPassword(password,hash){
-    return await bcrypt.compare(password,hash);
+
+// Function to validate a password by comparing it to a stored hash
+async function isValidPassword(password, hash) {
+    // Compare the provided password to the stored hash
+    return await bcrypt.compare(password, hash);
 }
 
-module.exports = {hashPassword, isValidPassword};
+// Export the 'hashPassword' and 'isValidPassword' functions for use in other parts of the application
+module.exports = { hashPassword, isValidPassword };
