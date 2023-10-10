d41150692a64f856452513decf18b5d7f2f9a3d7
DylanSmith <ST10114517@vcconnect.edu.za>
2023-10-10 13:43:29 +0200

    complex string

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const urlprefix = '/api';
 
-app.get('/', (req, res) => {
-    res.send('Hello World Express')
-})
+// Define a route for the root URL
+app.get(urlprefix + '/', (req, res) => {
+    res.send('Hello World');
+});
 
-app.get('/test', (req, res) => {
-    res.send('Hello World Express /test')
-})
+// Define a route for getting orders
+app.get(urlprefix + '/orders', (req, res) => {
+    const orders = [
+        {
+            id: "1",
+            name: "Orange"
+        },
+        {
+            id: "2",
+            name: "Banana"
+        },
+        {
+            id: "3",
+            name: "Pear"
+        }
+    ];
+
+    
+    res.json({
+        message: "Fruits",
+        orders: orders
+    });
+});
 
 module.exports = app;
