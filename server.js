97d3b308c4353dda3762a7cfd28ed54dbb653bed
DylanSmith <ST10114517@vcconnect.edu.za>
2023-10-10 13:22:54 +0200

    fixed test.http

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const https = require('http');
+const http = require('http');
 const fs = require('fs');
 const app = require('./app');
 
 const port = 3000;
 
-const server = https.createServer(
+const server = http.createServer(
     {
         key: fs.readFileSync('./keys/privatekey.pem'),
         cert: fs.readFileSync('./keys/certificate.pem')
